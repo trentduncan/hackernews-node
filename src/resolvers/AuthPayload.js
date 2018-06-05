a7d85a16ce85b12c0aea82b39414d1db7301ee73
trentduncan <trent.dunc@gmail.com>
2018-06-05 09:19:03 -0700

    refactored resolvers and added utils

---
+++ src/resolvers/AuthPayload.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function user(root, args, context, info) {
+  return context.db.query.user({ where: { id: root.user.id } }, info);
+}
+  
+module.exports = { user };
---
+++ src/resolvers/Query.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function feed(parent, args, context, info) {
+  return context.db.query.links({},info);
+}
+
+module.exports = {feed};
